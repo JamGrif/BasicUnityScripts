6400cff688391c9c332519279b9780c5242702c7
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-10-01 21:43:45 +0100

    Main

---
+++ VariablesAndFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+    void Start()
+    {
+        Debug.Log(MultiplyByTwo(myInt));
+
+
+    }
+
+    int MultiplyByTwo(int num)
+    {
+        return num * 2;
+    }
+
+
+
+}
